30233a971fd54cb2935e396f03ff1c41ae167d27
Jack <jack@ofspades.com>
2024-06-06 18:18:43 -0400

    Migrate to redis

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
 from flask import Flask, render_template, request, redirect, url_for, session
-from pymongo import MongoClient
 import os
+import redis
+import json
 
 app = Flask(__name__)
 app.secret_key = os.environ.get("SECRET_KEY", "ozempicisruiningeverything")
 
-# Set up MongoDB client
-uri = os.environ.get("DATABASE_URL")
-client = MongoClient(uri)
+# Set up Redis client
+uri = os.environ.get("REDIS_URL")
+redis_client = redis.from_url(uri)
 db = client['TeamNames0']
-team_names_collection = db['team_names']
-user_votes_collection = db['user_votes']
 
 @app.route('/', methods=['GET', 'POST'])
 def index():
     if request.method == 'POST':
         first_name = request.form['first_name']
         session['first_name'] = first_name
-        if user_votes_collection.find_one({'first_name': first_name}) is None:
-            user_votes_collection.insert_one({'first_name': first_name, 'votes': []})
+        if not redis_client.exists(f'user_votes:{first_name}'):
+            redis_client.set(f'user_votes:{first_name}', json.dumps([]))
         return redirect(url_for('vote'))
 
     return render_template('index.html')
@@ -29,24 +28,23 @@ def vote():
         return redirect(url_for('index'))
     
     first_name = session['first_name']
-    user_data = user_votes_collection.find_one({'first_name': first_name})
-    user_votes = user_data['votes'] if user_data else []
-    
+    user_votes = json.loads(redis_client.get(f'user_votes:{first_name}'))
+
     if request.method == 'POST':
         if 'team_name' in request.form:
             team_name = request.form['team_name']
-            if team_names_collection.find_one({'name': team_name}) is None:
-                team_names_collection.insert_one({'name': team_name, 'score': 0})
-        
+            if not redis_client.exists(f'team_name:{team_name}'):
+                redis_client.set(f'team_name:{team_name}', 0)
+
         else:
             team_name = request.form['vote']
             if len(user_votes) < 3 and team_name not in user_votes:
-                team_names_collection.update_one({'name': team_name}, {'$inc': {'score': 1}})
-                user_votes_collection.update_one({'first_name': first_name}, {'$push': {'votes': team_name}})
+                redis_client.incr(f'team_name:{team_name}')
                 user_votes.append(team_name)
-    
-    team_names = list(team_names_collection.find())
-    
+                redis_client.set(f'user_votes:{first_name}', json.dumps(user_votes))
+
+    team_names = [(key.split(':')[1], redis_client.get(key)) for key in redis_client.keys('team_name:*')]
+
     return render_template('vote.html', team_names=team_names, user_votes=user_votes)
 
 if __name__ == '__main__':
